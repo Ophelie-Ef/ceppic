e4b7e8b81d59d58835cd83c35fffc2d545187eee
Ophelie <freville.ophelie@neuf.fr>
2024-01-08 10:40:39 +0100

    Penser aux antislash en cas de mesg erreur !!!

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -6,20 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/formations')]
 class FormationsController extends AbstractController
 {
-    #[Route('/formations/{domaine}', 'formations', defaults: ['domaine' => ''])]
-    public function domaine(string $domaine): Response
+    #[Route('/', 'app_formations')]
+    public function index(): Response
     {
-        //     return new Response('Formation : '.$domaine);
-        // }
+        return $this->render('formations/index.html.twig');
+    }
 
-        // #[Route('/formations', 'formations')]
-        // public function index(): Response
-        // {
-        return $this->render(
-            'formations/index.html.twig',
-            ['domaine' => $domaine]
+    #[Route('/{page<\d+>}', name: 'app_formations_show', defaults:['page' => '1'] )]
+    public function show(int $page): Response
+    {
+        return $this->render('formations/show.html.twig',
+            [
+                'page' => $page
+            ]
         );
     }
 }
